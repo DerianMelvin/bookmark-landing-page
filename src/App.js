68db2670086059ec37b3fb3382c592d8cd18253c
Royson <roysoncien@gmail.com>
2021-10-20 21:45:08 +0700

    ganti app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Header from "./header/header";
+
 function App() {
-  return <div className="App">
-    <h1>Hello World</h1>
-  </div>;
+  return (
+
+    <div className="App">
+      <Header />
+    </div>
+  )
 }
 
 export default App;
