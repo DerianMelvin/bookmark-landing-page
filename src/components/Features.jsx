a0122c9aac4fd8f2b954b4f2f5e18102a1e7e0ee
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-10-21 23:34:54 +0800

    Create component Features - Import component SubHeading

---
+++ src/components/Features.jsx
@@ -0,0 +1,14 @@
+import SubHeading from "./SubHeading";
+
+const Features = () => {
+  return (
+    <div>
+      <SubHeading
+        subtitle="Features"
+        description="Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go."
+      />
+    </div>
+  );
+};
+
+export default Features;
