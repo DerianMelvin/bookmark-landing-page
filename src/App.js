b4be12e39a0c37c00f51a8cd8aec33fab4f64a32
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-10-21 23:36:04 +0800

    Replace imported component SubHeading with Features

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import SubHeading from "./components/SubHeading";
+import Features from "./components/Features";
 import Header from "./header/header";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SubHeading
-        subtitle="Features"
-        description="Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go."
-      />
+      <Features />
     </div>
   );
 }
